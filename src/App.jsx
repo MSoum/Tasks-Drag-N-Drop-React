105b7228266e955839cf836bd79714a2f72d41f0
Michael Soumah <mikesoum73@gmail.com>
2024-07-02 21:53:20 -0400

    Update : ajout de description dans le Readme.md

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { arrayMove, sortableKeyboardCoordinates, usese } from "@dnd-kit/sortable";
+
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  closestCorners,
+  TouchSensor,
+} from "@dnd-kit/core";
+
+import { Column } from "./Components/Column/Column";
+import './App.css';
+import { Input } from './Components/Input/Input';
+
+export default function App() {
+
+  const [tasks, setTasks] = useState([
+    { id: 1, title: "Add tests to homepage" },
+    { id: 2, title: "Fix styling in about section" },
+    { id: 3, title: "Learn how to center a div" },
+  ]);
+
+  const addTask = (title) => {
+    setTasks((tasks) => [...tasks, { id: tasks.length + 1, title }]);
+  };
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(TouchSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const getTaskPos = (id) => tasks.findIndex((task) => task.id === id);
+
+  const handleDragEnd = (event) => {
+    const { active, over } = event;
+
+    if (active.id === over.id) return;
+
+    setTasks((tasks) => {
+      const originalPos = getTaskPos(active.id);
+      const newPos = getTaskPos(over.id);
+
+      return arrayMove(tasks, originalPos, newPos);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1> My Tasks ✅</h1>
+      <DndContext sensors={sensors} onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
+        <Input onSubmit={addTask} />
+        <Column tasks={tasks}></Column>
+      </DndContext>
+    </div>
+  );
+}
